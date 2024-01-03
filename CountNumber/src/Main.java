a8f69de1b1bc306013c17650e7c0e50e566292a2
viettoan1020 <viettoann102003@gmail.com>
2024-01-03 13:42:38 +0700

    thuchanh3

---
+++ CountNumber/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Count count = new Count();
+        try {
+            while (count.getMyThread().isAlive()) {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread run is over");
+    }
+}
